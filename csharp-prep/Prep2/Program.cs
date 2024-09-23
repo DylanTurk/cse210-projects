14929a80cec15cd5c193b2246efd81e24b8b8674
DylanTurk <tur19010@byui.edu>
2024-09-22 19:15:55 -0600

    Wrote program to ask for you grade percent then return your letter grade and determine if you passed or failed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string askPercent = Console.ReadLine();
+
+        int percent = int.Parse(askPercent);
+        string letterGrade= "A";
+
+        if (percent >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percent >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letterGrade}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed, better luck next time.");
+        }
     }
 }
