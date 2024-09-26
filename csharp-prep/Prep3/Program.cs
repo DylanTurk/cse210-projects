f1116c102b4504c02eaa5d4827e55885385e59a6
DylanTurk <tur19010@byui.edu>
2024-09-25 23:15:53 -0600

    Mad lib and Magic Number Guessing

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Net;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Creating magic number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+
+        // Call and Response loop
+        int guess;
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess == magicNumber)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        } while (magicNumber != guess);
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Give me an adjective");
+        string adj = Console.ReadLine();
+        Console.WriteLine("Give me a color");
+        string color = Console.ReadLine();
+        Console.WriteLine("Give me a noun");
+        string noun1 = Console.ReadLine();
+        Console.WriteLine("Give me a plural noun");
+        string plural1 = Console.ReadLine();
+        Console.WriteLine("Give me a person's name");
+        string  name = Console.ReadLine();
+        Console.WriteLine("Give me a place");
+        string place = Console.ReadLine();
+        Console.WriteLine("Give me an adverb");
+        string adverb = Console.ReadLine();
+        Console.WriteLine("Give me a plural noun");
+        string plural2 = Console.ReadLine();
+        Console.WriteLine("Give me a noun");
+        string noun2 = Console.ReadLine();
+        Console.WriteLine("Give me a verb");
+        string verb = Console.ReadLine();
+        Console.WriteLine("Give me a plural noun");
+        string plural3 = Console.ReadLine();
+
+        //Console.WriteLine($"One {adj} day, I was walking my {color} pet {noun1} when {plural1} started flying from the sky!");
+        //Console.WriteLine($"I called my friend {name} and she said one just landed right on her {place}!");
+        //Console.WriteLine($"({adverb}, there was raining {plural2} as well and they were just going everywhere!");
+        //Console.WriteLine($"My {noun2} started {verb} and catching {plural3} with his mouth to eat.");
     }
 }
