9eca8a29718d9f0bcec4ea52600c97cebff9ef45
DylanTurk <tur19010@byui.edu>
2024-09-25 23:43:53 -0600

    Numbeers list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.WriteLine("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            numbers.Add(number);
+        } while (number != 0);
+
+        int total = numbers.Sum();
+        Console.WriteLine($"Your sum is: {total}");
+
+        int avg = total / numbers.Count;
+        Console.WriteLine($"The average is: {avg}");
+
+        int largest = numbers.Max();
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
