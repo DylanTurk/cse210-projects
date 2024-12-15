442a89725127284beea5d80081d4329f9fddeebb
DylanTurk <tur19010@byui.edu>
2024-12-15 05:24:11 -0700

    Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        Square square = new Square("Red", 5.0);
+
+        Rectangle rectangle = new Rectangle("Blue", 3.0, 4.0);
+
+        Circle circle = new Circle("Green", 2.0);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        Console.WriteLine("\nIterating through shapes in the list...");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetType().Name} - Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
